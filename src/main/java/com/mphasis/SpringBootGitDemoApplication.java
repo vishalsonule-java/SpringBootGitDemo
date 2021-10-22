b22ed7c3530621f60d2143ea222131b9c73eaf6a
vishalsonule-java <vishalsonule2@gmail.com>
2021-10-22 11:49:52 +0530

    initial commit

---
+++ src/main/java/com/mphasis/SpringBootGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mphasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mphasis/SpringBootGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
